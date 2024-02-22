6db01bfc18e7884391f662fd82581d5b5c7c7c79
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2024-02-22 14:17:36 +0100

    Finish back-end search fruit releases

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,22 +14,38 @@ use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
+#[Route('/home')]
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home', methods: ['GET'])]
-    public function index(Request $request, DiscogsAccess $api, string $fruit): Response
+    #[Route('/', name: 'app_home', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
+        if ($request->getMethod() === 'POST') {
+            return $this->extractFruitPost($request);
+        }
+
         return $this->render('home/index.html.twig', []);
     }
 
-    #[Route('/home/search', name: 'app_home_search', methods: ['GET', 'POST'])]
-    public function search(Request $request, DiscogsAccess $api): Response
-    {
-        $releases = $api->getSearchRelease(
-            $request->request->get("fruit_search"));
+    #[Route('/{fruit}', name: 'app_home_search', methods: ['GET', 'POST'])]
+    public function search(Request $request, DiscogsAccess $api, string $fruit): Response {
+        if ($request->getMethod() === 'POST') {
+            return $this->extractFruitPost($request);
+        }
+
+        $releases = $api->getSearchRelease($fruit);
 
         return $this->render('home/index.html.twig', [
             'releases' => $releases
         ]);
+    }
+
+
+    private function extractFruitPost(Request $request): Response {
+        $fruit = $request->request->get("fruit_search");
+
+        return $this->redirectToRoute('app_home_search',
+            ['fruit' => $fruit],
+            Response::HTTP_SEE_OTHER);
     }
 }
